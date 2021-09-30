444d4f273500f1c70216d3c05273663dca7c7fb4
Kygan <kygandao23@gmail.com>
2021-09-29 20:33:15 -0700

    started mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const { ApolloServer } = require("apollo-server");
+const { MongoClient } = require('mongodb');
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const client = new MongoClient('mongodb://localhost:27017'); 
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const app = async () => {
+    await client.connect().then(console.log('Mongo Connected')).catch(console.error);
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
+};
+
+app();
